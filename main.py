9093930576df4cd3e678e1e0581a3571c2ea5f11
sellareddy18 <shashanke7y@gmail.com>
2024-07-16 23:46:51 -0400

    simple python code that fetches user data

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from dotenv import load_dotenv
+import os
+from notion_client import Client
+from pprint import pprint
+
+
+load_dotenv()
+notion = Client(auth=os.getenv('NOTION_TOKEN'))
+
+list_users_response = notion.users.list()
+pprint(list_users_response)
